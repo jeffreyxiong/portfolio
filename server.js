2c1b67d2ae86feed9a56edfabdf1322cf23f42b2
jeffreyxiong <jeffreywxiong@gmail.com>
2018-11-15 07:43:41 -0800

    updated styles and assets

--- server.js
+++ server.js
@@ -1,27 +1,57 @@
 import express from 'express';
 import path from 'path';
+import whiskers from 'whiskers';
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname + '/dist'));
+app.set('views', __dirname + '/src/views');
+app.engine('.html', whiskers.__express);
 
 // basic routing
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+    res.render('layout.html', {
+        partials: {
+            body: 'index.html'
+        },
+        title: 'Jeffrey Xiong',
+        file: 'index',
+    });
 });
 
 app.get('/illustrations', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/illustrations.html'));
+    res.render('layout.html', {
+        partials: {
+            body: 'illustrations.html'
+        },
+        title: 'Illustrations | Jeffrey Xiong',
+        file: 'illustrations',
+    });
 });
 
 app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/projects.html'));
+    res.render('layout.html', {
+        partials: {
+            body: 'projects.html'
+        },
+        title: 'Projects | Jeffrey Xiong',
+        file: 'projects',
+    });
 });
 
 app.get('/photography', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/photography.html'));
+    res.render('layout.html', {
+        partials: {
+            body: 'photography.html'
+        },
+        title: 'Photography | Jeffrey Xiong',
+        file: 'photos',
+        photos: {
+            
+        }
+    });
 });
 
 // photo handling with DigitalOcean Spaces
