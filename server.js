5f94b16ffb2b179820fcc1e0342b16f7e6b6c4ad
jeffreyxiong <jeffreywxiong@gmail.com>
2019-01-26 21:33:13 -0800

    work

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'path';
 import whiskers from 'whiskers';
+import aws from 'aws-sdk';
+
+// routing setup
 
 const app = express();
 const port = 3000;
@@ -9,7 +12,14 @@ app.use(express.static(__dirname + '/dist'));
 app.set('views', __dirname + '/src/views');
 app.engine('.html', whiskers.__express);
 
-// basic routing
+// spaces endpoint + S3 setup
+
+const ocean = new aws.Endpoint('https://jeffers.sfo2.cdn.digitaloceanspaces.com');
+const s3 = new aws.S3({
+    endpoint: ocean
+});
+
+// routes
 
 app.get('/', (req, res) => {
     res.render('layout.html', {
@@ -27,7 +37,7 @@ app.get('/illustrations', (req, res) => {
             body: 'illustrations.html'
         },
         title: 'Illustrations | Jeffrey Xiong',
-        file: 'illustrations',
+        file: 'index',
     });
 });
 
@@ -37,27 +47,50 @@ app.get('/projects', (req, res) => {
             body: 'projects.html'
         },
         title: 'Projects | Jeffrey Xiong',
-        file: 'projects',
+        file: 'index',
     });
 });
 
 app.get('/photography', (req, res) => {
+
+    var natureFiles = getNature();
+
     res.render('layout.html', {
         partials: {
             body: 'photography.html'
         },
         title: 'Photography | Jeffrey Xiong',
         file: 'photos',
-        photos: {
-            
-        }
+        photos: natureFiles
     });
 });
 
 // photo handling with DigitalOcean Spaces
 
-app.get('/photography/landscape',(req, res) => {
-    
-});
+const params = (name) => {
+    return {
+        bucket: name
+    }
+}
+
+const getNature = () => {
+    var files = [];
+
+    console.log("hello");
+
+    s3.listObjectsV2(params('nature'), (error, data) => {
+        if (!error) {
+            data.Contents.forEach((element) => {
+                files.push({
+                    filename: element.key
+                })
+            })
+        } else {
+            console.log(error);
+        }
+    });
+
+    return files;
+}
 
 app.listen(port);
